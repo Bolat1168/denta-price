e0f188b9aaf0ccab3d28de4a44a94cd41bcfd995
Bolat1168 <164166102+Bolat1168@users.noreply.github.com>
2026-04-26 10:59:59 +0500

    fix: remove conflicting configs and reset next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,5 @@
 ﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['firebasestorage.googleapis.com', 'lh3.googleusercontent.com'],
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-};
-
-module.exports = nextConfig;
+}
+module.exports = nextConfig
